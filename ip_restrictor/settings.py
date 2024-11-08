1c7b5062b1fac76add65b00ee166d867cbd07ff0
Alireza Fathi <alireza.fathi@snappshop.ir>
2024-11-08 20:44:26 +0330

    initial: add project structure

---
+++ ip_restrictor/settings.py
@@ -0,0 +1,6 @@
+WHITELIST_IPS = ['127.0.0.1', '192.168.1.100']
+BLACKLIST_IPS = ['10.0.0.5', '10.0.0.6']
+
+# Default restriction mode for all views
+# Options: "whitelist", "blacklist", or False
+DEFAULT_IP_RESTRICTION_MODE = False
